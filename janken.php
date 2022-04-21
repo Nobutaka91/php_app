4ef9cd23d2f89faecb186661146f92c293756374
otamasan8 <hatarakuotamasan8@outlook.jp>
2022-04-21 10:44:28 +0900

    じゃんけんプログラム

--- janken.php
+++ janken.php
@@ -7,6 +7,18 @@
     <title>janken</title>
 </head>
 <body>
-    
+    <?php 
+        print"<p>じゃんけんしよう</p>";
+        $a = rand(1,3);
+        if($a == 1) {
+            print "<img src='img/gu.png'>";
+        }
+        if($a == 2) {
+            print "<img src='img/choki.png'>";
+        }
+        if($a == 3) {
+            print "<img src='img/pa.png'>";
+        }
+    ?>
 </body>
 </html>
